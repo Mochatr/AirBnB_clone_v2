9d383f4215c6ed719bde8ee3c37a618909a0243a
Mochatr <mohamedchatr8@gmail.com>
2024-02-04 15:52:43 -0800

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,28 +14,37 @@ env.hosts = ['54.146.83.181', '54.210.196.216']
 def do_deploy(archive_path):
     if not isfile(archive_path):
         return False
+    try:
+        
+        # Upload the archive
+        put(archive_path, '/tmp/')
 
-    # Upload the archive
-    put(archive_path, '/tmp/')
+        # Extract the filename without extension
+        file_name = archive_path.split("/")[-1]
+        name = file_name.split(".")[0]
 
-    # Extract the filename without extension
-    file_name = archive_path.split("/")[-1]
-    name = file_name.split(".")[0]
+        # Define the directories
+        remote_path = "/data/web_static/releases/"
 
-    # Define the directories
-    remote_path = "/data/web_static/releases/{}/".format(name)
+        # Create directory
+        run("mkdir -p {}{}".format(remote_path, name))
 
-    # Create directory
-    run("mkdir -p {}".format(remote_path))
-    # Uncompress the archive
-    run("tar -xzf /tmp/{} -C {}".format(file_name, remote_path))
-    # Delete archive
-    run("rm /tmp/{}".format(file_name))
+        # Uncompress the archive
+        run("tar -xzf /tmp/{} -C {}{}".format(file_name, remote_path, name))
 
-    # Delete the symbolic link
-    run("rm -rf /data/web_static/current")
-    # Create a new one
-    run("ln -s {} /data/web_static/current".format(remote_path))
+        run("rm /tmp/{}".format(file_name))
 
-    print("New version deployed!")
-    return True
+        # Move web static content
+        run("mv {0}{1}/web_static/* {0}{1}/".format(remote_path, name))
+        # Remove the original folder
+        run("rm -rf {}{}/web_static".format(remote_path, name))
+
+        # Delete the symbolic link
+        run("rm -rf /data/web_static/current")
+
+        # Create a new one
+        run("ln -s {}{}/ /data/web_static/current".format(remote_path, name))
+
+        return True
+    except Exception as e:
+        return False
