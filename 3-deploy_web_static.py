6cd76871dbe99dac86801a129e1a27e046ad983f
Mochatr <mohamedchatr8@gmail.com>
2024-02-04 16:27:38 -0800

    update

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,46 +8,59 @@ env.hosts = ['54.87.205.91', '54.87.240.9']
 
 
 def do_pack():
-    """generates a tgz archive"""
+    """This function generates a tgz archive"""
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        date_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(date_time)
+
         local("mkdir -p versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
+        local("tar -cvzf {} web_static".format(archive_path))
+
+        return archive_path
     except Exception as e:
         return None
 
 
 def do_deploy(archive_path):
-    """distributes archive remotely"""
-    if exists(archive_path) is False:
+    if not isfile(archive_path):
         return False
     try:
-        """Extract Filename and No Extension"""
+
+        # Upload the archive
+        put(archive_path, '/tmp/')
+
+        # Extract the filename without extension
         file_name = archive_path.split("/")[-1]
         name = file_name.split(".")[0]
-        """Prepare Path and Upload Archive"""
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        """Create Release Directory and Extract Archive"""
-        run('mkdir -p {}{}/'.format(path, name))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, name))
-        """Clean Up Temporary Files"""
-        run('rm /tmp/{}'.format(file_name))
-        """Move Web Static Content and Remove Original Folder"""
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, name))
-        run('rm -rf {}{}/web_static'.format(path, name))
-        """Remove Old Current Link and Create New Symbolic Link"""
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, name))
+
+        # Define the directories
+        remote_path = "/data/web_static/releases/"
+
+        # Create directory
+        run("mkdir -p {}{}".format(remote_path, name))
+
+        # Uncompress the archive
+        run("tar -xzf /tmp/{} -C {}{}".format(file_name, remote_path, name))
+
+        run("rm /tmp/{}".format(file_name))
+
+        # Move web static content
+        run("mv {0}{1}/web_static/* {0}{1}/".format(remote_path, name))
+        # Remove the original folder
+        run("rm -rf {}{}/web_static".format(remote_path, name))
+
+        # Delete the symbolic link
+        run("rm -rf /data/web_static/current")
+
+        # Create a new one
+        run("ln -s {}{}/ /data/web_static/current".format(remote_path, name))
         return True
     except Exception as e:
         return False
 
 
 def deploy():
-    """full deploy"""
+    """deploy"""
     archive_path = do_pack()
     if archive_path is None:
         return False
